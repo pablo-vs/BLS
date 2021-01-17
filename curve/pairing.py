453a8a0491b754821da1758f75294ed1b3a31d72
AndreaPCalvin <72571601+AndreaPCalvin@users.noreply.github.com>
2021-01-17 12:55:59 +0100

    Update pairing.py

--- curve/pairing.py
+++ curve/pairing.py
@@ -1,12 +1,11 @@
 from curve.curve import Point, CurvePoint, G1, curve_order
 from curve.curve_fields import FQ, FQ2, FQ12, BLS12_381_FQ_MODULUS as field_modulus
 
-
+#number of iterations that Miller's loop algorithm requires
 ate_loop_count = 15132376222941642752
 log_ate_loop_count = 62
 
-
-
+#returns the line between P1 and P2 evaluated in Q
 def linefunc(P1: CurvePoint, P2: CurvePoint, Q: Point) -> FQ:
     if P1.is_infinite() or P2.is_infinite() or Q.is_infinite():
         raise ValueError("Can't compute line function on infinite point")
@@ -24,12 +23,12 @@ def linefunc(P1: CurvePoint, P2: CurvePoint, Q: Point) -> FQ:
     else:
         return xq - x1
 
-
+#embeds a FQ point into a FQ12 point
 def embed_FQ12(P: Point) -> Point:
     x, y = P
     return type(P)(FQ12(x), FQ12(y))
 
-
+#algorithm which calculates pairing
 def miller_loop(P: CurvePoint, Q: CurvePoint) -> FQ12:
     if P.is_infinite() or Q.is_infinite():
         return FQ12.one()
